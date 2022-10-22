084bcb7391551ce61757f34b5057efa2bb8398e0
jcarloshg <carlosj12336@gmail.com>
2022-10-21 19:00:18 -0500

    point sAve

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,20 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Heroe, Publisher } from '../../interfaces/heroe.interfece';
+import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class AgregarComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    },
+  ];
+
+  heroe: Heroe = {
+    superhero: '',
+    publisher: Publisher.DCComics,
+    alter_ego: '',
+    first_appearance: '',
+    characters: '',
+    alt_img: '',
+  };
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private heroesService: HeroesService,
+  ) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(({ id }) => {
-      console.log(id);
-    })
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.heroesService.getHeroe(id))
+      )
+      .subscribe(heroe => this.heroe = heroe)
+  }
+
+  registrarHeroe() {
+    if (this.heroe.superhero.trim().length == 0) return;
+    this.heroesService
+      .crearHeroe(this.heroe)
+      .subscribe((heroe) => console.log(heroe));
   }
 
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -11,6 +11,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   exports: [
@@ -25,6 +26,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatProgressSpinnerModule,
     MatSidenavModule,
     MatToolbarModule,
+    MatSelectModule,
   ]
 })
 export class MaterialModule { }
